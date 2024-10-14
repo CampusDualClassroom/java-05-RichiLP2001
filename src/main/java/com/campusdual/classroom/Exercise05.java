f7d7485815208607198d1a89ea152a07bfa3f75a
Ricardo Lopez Peña <ricardoslopezs2001@gmail.com>
2024-10-14 17:43:04 +0200

    Java, ejercicio 5. ¿Por queno pasa los test si d los resultados?

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -1,7 +1,15 @@
 package com.campusdual.classroom;
 
+import jdk.jshell.execution.Util;
+
+import java.util.Scanner;
+
 public class Exercise05 {
     public static void main(String[] args) {
+    int n1 = 10;
+    int n2 = 4;
+
+    checkMultiply(n1,n2);
 
     }
 
@@ -10,19 +18,36 @@ public class Exercise05 {
     // Si el valor es mayor o igual, tiene que devolver el siguiente mensaje: X es mayor o igual que Y
     public static void checkLessOrGreaterEqual(int n1, int n2) {
 
+
+        if (n1>=n2){
+            System.out.println(n1+" es mayor o igual que "+n2);
+        }else {
+            System.out.println(n2+" es menor que "+n1);
+        }
+
     }
 
     //TODO ↓
     // Si es múltiplo, tiene que devolver el siguiente mensaje: X es múltiplo de Y
     // Si no es múltiplo, tiene que devolver el siguiente mensaje: X no es múltiplo de Y
     public static void checkMultiply(int n1, int n2) {
-
+    if (n2 % n1 ==0){
+        System.out.println("n1 multiply of n2");
+    }else {
+        System.out.println(" n1 not multiply of n2");
+    }
     }
 
     //TODO ↓
     // Si es negativo, tiene que devolver el siguiente mensaje: X es negativo.
     // Si es positivo o cero, tiene que devolver el siguiente mensaje: X es positivo.
     public static void checkPositiveNegative(int num) {
+        // main --> int num= cualquiera.
+        if (num>=0){
+            System.out.println(num+" es positivo");
+        }else{
+            System.out.println(num+" es negativo");
+        }
         
     }
 }
